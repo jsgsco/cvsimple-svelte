d1706f18e1a625f298b1bbd8e0924b310daf6d78
Jhon Steven Saavedra <jhon.giron@uniminuto.edu.co>
2021-07-31 01:20:36 -0500

    Fix SEO

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,5 @@
 <svelte:head>
 	<title>Jhon's Ranch</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
     <link rel="shortcut icon" href="https://i.imgur.com/4SzTP0s.png">
 	<meta name="description" content="Jhon's Ranch, website personal">
 	<meta name="keywords" content="Svelte">
